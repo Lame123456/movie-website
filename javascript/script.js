9690c0bb8e2b727e6d8e739b1bf4a1b1325535b0
Lame123456 <159425121+Lame123456@users.noreply.github.com>
2024-07-13 00:19:48 +0200

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,48 @@
+'use strict';
 
+// variables for navbar menu toggle
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+const navbarMenuBtn = document.querySelector('.navbar-menu-btn');
+
+// variables for navbar search toggle
+const navbarForm = document.querySelector('.navbar-form');
+const navbarFormCloseBtn = document.querySelector('.navbar-form-close');
+const navbarSearchBtn = document.querySelector('.navbar-search-btn');
+
+
+// navbar menu toggle function
+function navIsActive() {
+  header.classList.toggle('active');
+  nav.classList.toggle('active');
+  navbarMenuBtn.classList.toggle('active');
+}
+
+navbarMenuBtn.addEventListener('click', navIsActive);
+
+
+
+// navbar search toggle function
+const searchBarIsActive = () => navbarForm.classList.toggle('active');
+
+navbarSearchBtn.addEventListener('click', searchBarIsActive);
+navbarFormCloseBtn.addEventListener('click', searchBarIsActive);
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
